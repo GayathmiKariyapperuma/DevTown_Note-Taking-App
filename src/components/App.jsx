7548e647418ce50e1e8ecc5ba35d556efaee2785
GS Kariyapperuma <gayathmikariyapperuma@gmail.com>
2022-08-08 19:21:33 +0000

    Import Notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Notes from "./Notes";
 
 function App() {
   return( 
   <div>
       <Header/>
+      <Notes/>
       <Footer/>
     </div>
   )
